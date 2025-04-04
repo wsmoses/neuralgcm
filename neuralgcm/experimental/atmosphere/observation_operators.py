61461a3fe6db621e62fbc6056c094cbba863d3f5
Dmitrii Kochkov <dkochkov@google.com>
2025-04-04 14:02:36 -0700

    Added SigmaLevelObservationOperator that mirrors PressureLevelObservationOperator with the goal of using these explicitly in the sigma/pressure level models explicitly.
    PiperOrigin-RevId: 744062557

--- neuralgcm/experimental/atmosphere/observation_operators.py
+++ neuralgcm/experimental/atmosphere/observation_operators.py
@@ -143,3 +143,108 @@ class PressureLevelObservationOperator(
     return observation_operators.DataObservationOperator(
         observations_fields | {'time': time}
     ).observe(inputs, query)
+
+
+@dataclasses.dataclass
+class SigmaLevelObservationOperator(
+    observation_operators.ObservationOperator
+):
+  """Operator that predicts sigma-level observations on lon-lat grids.
+
+  This operator is compatible with models that use primitive equation
+  representation of the atmospheric state in spherical harmonics basis. The
+  sigma-level observations are computed by converting the state to
+  velocity/temperature/geopotential representation and then linearly
+  interpolating to desired sigma levels using combination of linear and constant
+  extrapolation strategies. If `observation_correction` mapping is specified,
+  an additional correction is added to the predicted observations in lon-lat
+  space.
+  """
+
+  primitive_equation: equations.PrimitiveEquations
+  orography: orographies.ModalOrography
+  sigma_levels: coordinates.SigmaLevels
+  sim_units: units.SimUnits
+  tracer_names: Sequence[str]
+  observation_correction: pytree_mappings.CoordsStateMapping | None
+  mesh: parallelism.Mesh
+
+  def observe(
+      self,
+      inputs: dict[str, cx.Field],
+      query: dict[str, cx.Field | cx.Coordinate],
+  ) -> dict[str, cx.Field]:
+    """Observes fields in `query` on sigma/lon/lat coords from YLM `inputs`.
+
+    Args:
+      inputs: A dictionary of fields in spherical harmonic representation.
+      query: A dictionary specifying coordinates of the queried fields.
+
+    Returns:
+      Predicted fields matching the `query`.
+    """
+    inputs = copy.copy(inputs)  # avoid mutating inputs.
+    time = inputs.pop('time')
+    input_coords = cx.get_coordinate(inputs['divergence'])
+    # TODO(dkochkov): try to remove use of DinosaurCoordinates where possible,
+    # as reconstructing them from axes results in a CartesianProduct instance
+    # due to inheritance from cx.CartesianProduct.
+    input_coords = coordinates.DinosaurCoordinates(
+        vertical=input_coords.coordinates[0],
+        horizontal=cx.compose_coordinates(*input_coords.coordinates[1:])
+    )
+    grid = input_coords.horizontal.to_lon_lat_grid()
+    target_coords = coordinates.DinosaurCoordinates(
+        horizontal=grid, vertical=self.sigma_levels
+    )
+    inputs = jax.tree.map(lambda x: x.data, inputs, is_leaf=cx.is_field)
+    # TODO(dkochkov): make primitive_equation_to_uvtz work with flat structure
+    # to avoid the need to nesting tracers.
+    tracers = {k: inputs.pop(k) for k in self.tracer_names}
+    # TODO(dkochkov): standardize around not having dummy dimension for surface
+    # variables by adding expand_dims transform to features.
+    inputs['log_surface_pressure'] = (
+        inputs['log_surface_pressure'][jnp.newaxis, ...]
+    )
+    source_state = dinosaur_primitive_equations.State(
+        **(inputs | {'tracers': tracers})
+    )
+    # TODO(dkochkov): pass temperature instead of temperature_variation as
+    # prognostic fields in observer to remove this dependency.
+    interpolated_state = state_conversion.primitive_equations_to_sigma(
+        source_state=source_state,
+        input_coords=input_coords,
+        primitive_equations=self.primitive_equation,
+        orography=self.orography,
+        target_coords=target_coords,
+        sim_units=self.sim_units,
+        mesh=self.mesh,
+    )
+    interpolated_state = parallelism.with_physics_sharding(
+        self.mesh, interpolated_state
+    )
+    if self.observation_correction is not None:
+      # TODO(dkochkov): make this unnested as we move towards using
+      # dict[str, cx.Field] for internal representation.
+      source_dict = source_state.asdict() | {'time': time.data}
+      source_dict = parallelism.with_dycore_sharding(self.mesh, source_dict)
+      correction = self.observation_correction(source_dict)
+      correction = pytree_utils.replace_with_matching_or_default(
+          interpolated_state, correction
+      )
+      correction = parallelism.with_physics_sharding(self.mesh, correction)
+      add_fn = lambda x, y: x + y if y is not None else x
+      interpolated_state = jax.tree.map(
+          add_fn, interpolated_state, correction
+      )
+
+    observations_fields = {
+        k: cx.wrap(v, target_coords)
+        for k, v in interpolated_state.items() if k != 'surface_pressure'
+    }
+    observations_fields['surface_pressure'] = cx.wrap(
+        jnp.squeeze(interpolated_state['surface_pressure'], axis=0), grid
+    )
+    return observation_operators.DataObservationOperator(
+        observations_fields | {'time': time}
+    ).observe(inputs, query)
--- neuralgcm/experimental/atmosphere/observation_operators_test.py
+++ neuralgcm/experimental/atmosphere/observation_operators_test.py
@@ -32,59 +32,83 @@ import numpy as np
 
 class ObservationOperatorsTest(parameterized.TestCase):
 
-  def test_returns_pressure_level_outputs(self):
-    sh_grid = coordinates.SphericalHarmonicGrid.T21()
-    grid = coordinates.LonLatGrid.T21()
-    sigma_levels = coordinates.SigmaLevels.equidistant(12)
-    pressure_levels = coordinates.PressureLevels.with_13_era5_levels()
-    source_coords = coordinates.DinosaurCoordinates(
-        horizontal=sh_grid, vertical=sigma_levels
+  def setUp(self):
+    super().setUp()
+    n_sigma = 12
+    self.sh_grid = coordinates.SphericalHarmonicGrid.T21()
+    self.grid = coordinates.LonLatGrid.T21()
+    self.input_sigma_levels = coordinates.SigmaLevels.equidistant(n_sigma)
+    self.source_coords = coordinates.DinosaurCoordinates(
+        horizontal=self.sh_grid, vertical=self.input_sigma_levels
     )
-    target_coords = coordinates.DinosaurCoordinates(
-        horizontal=grid, vertical=pressure_levels
-    )
-    sim_units = units.DEFAULT_UNITS
-    mesh = parallelism.Mesh(None)
-    ref_temperatures = np.linspace(220, 250, num=sigma_levels.shape[0])
-    orography_module = orographies.ModalOrography(
-        grid=grid,
-        mesh=mesh,
+    self.sim_units = units.DEFAULT_UNITS
+    self.mesh = parallelism.Mesh(None)
+    self.orography_module = orographies.ModalOrography(
+        grid=self.grid,
+        mesh=self.mesh,
         rngs=nnx.Rngs(0),
     )
-    primitive_equations = equations.PrimitiveEquations(
-        coords=source_coords,
-        sim_units=sim_units,
-        reference_temperatures=ref_temperatures,
-        orography_module=orography_module,
-        mesh=mesh,
+    self.ref_temperatures = np.linspace(220, 250, num=n_sigma)
+    self.primitive_equations = equations.PrimitiveEquations(
+        coords=self.source_coords,
+        sim_units=self.sim_units,
+        reference_temperatures=self.ref_temperatures,
+        orography_module=self.orography_module,
+        mesh=self.mesh,
     )
-    operator = observation_operators.PressureLevelObservationOperator(
-        primitive_equations,
-        orography_module,
-        pressure_levels=pressure_levels,
-        sim_units=sim_units,
-        observation_correction=None,
-        tracer_names=['specific_humidity'],
-        mesh=mesh,
-    )
-    prognostic_fields = {
-        'divergence': cx.wrap(np.zeros(source_coords.shape), source_coords),
-        'vorticity': cx.wrap(np.zeros(source_coords.shape), source_coords),
-        'specific_humidity': cx.wrap(
-            np.zeros(source_coords.shape), source_coords
-        ),
-        'temperature_variation': cx.wrap(
-            np.zeros(source_coords.shape), source_coords
-        ),
-        'log_surface_pressure': cx.wrap(np.zeros(sh_grid.shape), sh_grid),
+    zero_like = lambda c: cx.wrap(np.zeros(c.shape), c)
+    self.prognostic_fields = {
+        'divergence': zero_like(self.source_coords),
+        'vorticity': zero_like(self.source_coords),
+        'specific_humidity': zero_like(self.source_coords),
+        'temperature_variation': zero_like(self.source_coords),
+        'log_surface_pressure': zero_like(self.sh_grid),
         'time': cx.wrap(jdt.to_datetime('2001-01-01')),
     }
+
+  def test_returns_pressure_level_outputs(self):
+    pressure_levels = coordinates.PressureLevels.with_13_era5_levels()
+    target_coords = coordinates.DinosaurCoordinates(
+        horizontal=self.grid, vertical=pressure_levels
+    )
+    operator = observation_operators.PressureLevelObservationOperator(
+        self.primitive_equations,
+        self.orography_module,
+        pressure_levels=pressure_levels,
+        sim_units=self.sim_units,
+        observation_correction=None,
+        tracer_names=['specific_humidity'],
+        mesh=self.mesh,
+    )
     query = {
         'temperature': target_coords,
         'u_component_of_wind': target_coords,
         'specific_humidity': target_coords,
     }
-    actual = operator.observe(inputs=prognostic_fields, query=query)
+    actual = operator.observe(inputs=self.prognostic_fields, query=query)
+    for key in query:
+      self.assertEqual(cx.get_coordinate(actual[key]), query[key])
+
+  def test_returns_sigma_level_outputs(self):
+    target_sigma_levels = coordinates.SigmaLevels.equidistant(10)
+    target_coords = coordinates.DinosaurCoordinates(
+        horizontal=self.grid, vertical=target_sigma_levels
+    )
+    operator = observation_operators.SigmaLevelObservationOperator(
+        self.primitive_equations,
+        self.orography_module,
+        sigma_levels=target_sigma_levels,
+        sim_units=self.sim_units,
+        observation_correction=None,
+        tracer_names=['specific_humidity'],
+        mesh=self.mesh,
+    )
+    query = {
+        'temperature': target_coords,
+        'u_component_of_wind': target_coords,
+        'specific_humidity': target_coords,
+    }
+    actual = operator.observe(inputs=self.prognostic_fields, query=query)
     for key in query:
       self.assertEqual(cx.get_coordinate(actual[key]), query[key])
 

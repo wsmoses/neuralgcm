132a3ae33865321f60be8ee730a5d03508964bcc
Dmitrii Kochkov <dkochkov@google.com>
2025-05-12 08:19:23 -0700

    Added SphericalHarmonicsTransform as a step towards parameterizing modules that operate in both spaces with these objects instead of LonLatGrid SphericalHarmonicGrid. This will allow us to simplify coordinate classes to contain only data layout relevant information.
    PiperOrigin-RevId: 757771814

---
+++ neuralgcm/experimental/core/spherical_transforms.py
@@ -0,0 +1,120 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Defines objects that transform between nodal and modal grids."""
+
+import dataclasses
+from typing import overload
+
+from dinosaur import spherical_harmonic
+import jax
+from neuralgcm.experimental import coordax as cx
+from neuralgcm.experimental.core import coordinates
+from neuralgcm.experimental.core import parallelism
+from neuralgcm.experimental.core import typing
+
+
+# TODO(dkochkov): Consider dropping nodal_grid and modal_grid properties and
+# instead have them as arguments when grids contain explicit padding details.
+
+
+@dataclasses.dataclass(frozen=True)
+class SphericalHarmonicsTransform:
+  """Spherical harmonic transform specified by grids and parallelism mesh."""
+
+  lon_lat_grid: coordinates.LonLatGrid
+  ylm_grid: coordinates.SphericalHarmonicGrid
+  mesh: parallelism.Mesh
+  partition_schema_key: parallelism.Schema | None
+  level_key: str = 'level'
+  longitude_key: str = 'longitude'
+  latitude_key: str = 'latitude'
+  radius: float = 1.0
+
+  @property
+  def dinosaur_spmd_mesh(self) -> jax.sharding.Mesh | None:
+    """Returns the SPMD mesh transformed to Dinosaur convention."""
+    dims_to_axes = {
+        self.level_key: 'z',
+        self.longitude_key: 'x',
+        self.latitude_key: 'y',
+    }
+    return self.mesh.rearrange_spmd_mesh(
+        self.partition_schema_key, dims_to_axes
+    )
+
+  @property
+  def dinosaur_grid(self) -> spherical_harmonic.Grid:
+    return spherical_harmonic.Grid(
+        longitude_wavenumbers=self.ylm_grid.longitude_wavenumbers,
+        total_wavenumbers=self.ylm_grid.total_wavenumbers,
+        longitude_nodes=self.lon_lat_grid.longitude_nodes,
+        latitude_nodes=self.lon_lat_grid.latitude_nodes,
+        longitude_offset=self.lon_lat_grid.longitude_offset,
+        latitude_spacing=self.lon_lat_grid.latitude_spacing,
+        radius=self.radius,
+        spherical_harmonics_impl=self.ylm_grid.spherical_harmonics_impl,
+        spmd_mesh=self.dinosaur_spmd_mesh,
+    )
+
+  @property
+  def nodal_grid(self) -> coordinates.LonLatGrid:
+    return coordinates.LonLatGrid.from_dinosaur_grid(self.dinosaur_grid)
+
+  @property
+  def modal_grid(self) -> coordinates.SphericalHarmonicGrid:
+    return coordinates.SphericalHarmonicGrid.from_dinosaur_grid(
+        self.dinosaur_grid
+    )
+
+  def to_modal_array(self, x: typing.Array) -> typing.Array:
+    """Converts a nodal array to a modal array."""
+    return self.dinosaur_grid.to_modal(x)
+
+  def to_nodal_array(self, x: typing.Array) -> typing.Array:
+    """Converts a modal array to a nodal array."""
+    return self.dinosaur_grid.to_nodal(x)
+
+  @overload
+  def to_modal(self, x: cx.Field) -> cx.Field:
+    ...
+
+  @overload
+  def to_modal(self, x: dict[str, cx.Field]) -> dict[str, cx.Field]:
+    ...
+
+  def to_modal(
+      self, x: cx.Field | dict[str, cx.Field]
+  ) -> cx.Field | dict[str, cx.Field]:
+    """Converts a nodal field(s) to a modal field(s)."""
+    if isinstance(x, dict):
+      return {k: self.to_modal(v) for k, v in x.items()}
+    modal = cx.cmap(self.to_modal_array)(x.untag(self.nodal_grid))
+    return modal.tag(self.modal_grid)
+
+  @overload
+  def to_nodal(self, x: cx.Field) -> cx.Field:
+    ...
+
+  @overload
+  def to_nodal(self, x: dict[str, cx.Field]) -> dict[str, cx.Field]:
+    ...
+
+  def to_nodal(
+      self, x: cx.Field | dict[str, cx.Field]
+  ) -> cx.Field | dict[str, cx.Field]:
+    """Converts a modal field(s) to a nodal field(s)."""
+    if isinstance(x, dict):
+      return {k: self.to_nodal(v) for k, v in x.items()}
+    nodal = cx.cmap(self.to_nodal_array)(x.untag(self.modal_grid))
+    return nodal.tag(self.nodal_grid)
---
+++ neuralgcm/experimental/core/spherical_transforms_test.py
@@ -0,0 +1,111 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from absl.testing import absltest
+from absl.testing import parameterized
+import chex
+from dinosaur import spherical_harmonic
+import jax
+from jax import config  # pylint: disable=g-importing-member
+from neuralgcm.experimental.core import coordinates
+from neuralgcm.experimental.core import parallelism
+from neuralgcm.experimental.core import spherical_transforms
+from neuralgcm.experimental.core import typing
+import numpy as np
+
+
+class SphericalTransformsTest(parameterized.TestCase):
+  """Tests SphericalHarmonicsTransform methods."""
+
+  @parameterized.parameters(
+      dict(
+          modal_input_array=np.arange(44 * 23).reshape((44, 23)),
+          ylm_grid=coordinates.SphericalHarmonicGrid.T21(),
+          lon_lat_grid=coordinates.LonLatGrid.T21(),
+      ),
+      dict(
+          modal_input_array=np.arange(128 * 65).reshape((128, 65)),
+          ylm_grid=coordinates.SphericalHarmonicGrid.TL63(),
+          lon_lat_grid=coordinates.LonLatGrid.TL63(),
+      ),
+      dict(
+          modal_input_array=np.arange(128 * 65).reshape((128, 65)),
+          ylm_grid=coordinates.SphericalHarmonicGrid.TL63(
+              spherical_harmonics_impl=spherical_harmonic.FastSphericalHarmonics
+          ),
+          lon_lat_grid=coordinates.LonLatGrid.TL63(
+              spherical_harmonics_impl=spherical_harmonic.RealSphericalHarmonics
+          ),
+      ),
+  )
+  def test_array_transforms(
+      self,
+      modal_input_array: typing.Array,
+      ylm_grid: coordinates.SphericalHarmonicGrid,
+      lon_lat_grid: coordinates.LonLatGrid,
+  ):
+    """Tests that SphericalHarmonicsTransform is equivalent to dinosaur."""
+    mesh = parallelism.Mesh(spmd_mesh=None)
+    transform = spherical_transforms.SphericalHarmonicsTransform(
+        lon_lat_grid=lon_lat_grid,
+        ylm_grid=ylm_grid,
+        mesh=mesh,
+        partition_schema_key='spatial',  # unused.
+    )
+    dinosaur_grid = spherical_harmonic.Grid(
+        longitude_wavenumbers=ylm_grid.longitude_wavenumbers,
+        total_wavenumbers=ylm_grid.total_wavenumbers,
+        longitude_nodes=lon_lat_grid.longitude_nodes,
+        latitude_nodes=lon_lat_grid.latitude_nodes,
+        longitude_offset=lon_lat_grid.longitude_offset,
+        latitude_spacing=lon_lat_grid.latitude_spacing,
+        radius=1.0,
+        spherical_harmonics_impl=ylm_grid.spherical_harmonics_impl,
+        spmd_mesh=None,
+    )
+    nodal_array = transform.to_nodal_array(modal_input_array)
+    expected_nodal_array = dinosaur_grid.to_nodal(modal_input_array)
+    np.testing.assert_allclose(nodal_array, expected_nodal_array)
+    # back to modal transform.
+    modal_array = transform.to_modal_array(nodal_array)
+    expected_modal_array = dinosaur_grid.to_modal(expected_nodal_array)
+    np.testing.assert_allclose(modal_array, expected_modal_array)
+
+  def test_modal_grid_property_is_padded(self):
+    ylm_grid = coordinates.SphericalHarmonicGrid.T21()
+    lon_lat_grid = coordinates.LonLatGrid.T21()
+    spmd_mesh = jax.sharding.Mesh(
+        devices=np.array(jax.devices()).reshape((2, 2, 2)),
+        axis_names=['z', 'x', 'y'],
+    )
+    mesh = parallelism.Mesh(
+        spmd_mesh=spmd_mesh,
+        field_partitions={'spatial': {'longitude': 'x', 'latitude': 'y'}},
+    )
+    transform = spherical_transforms.SphericalHarmonicsTransform(
+        lon_lat_grid=lon_lat_grid,
+        ylm_grid=ylm_grid,
+        mesh=mesh,
+        partition_schema_key='spatial',
+    )
+    padded_modal_grid = transform.modal_grid
+    self.assertIsInstance(padded_modal_grid, coordinates.SphericalHarmonicGrid)
+    self.assertEqual(transform.modal_grid.total_wavenumbers, 23)
+    self.assertEqual(transform.modal_grid.shape, (64, 32))
+
+
+if __name__ == '__main__':
+  chex.set_n_cpu_devices(8)
+  config.update('jax_traceback_filtering', 'off')
+  absltest.main()

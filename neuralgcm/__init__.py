86eeaf9f394c5fb09fa6839080fc1a736ed3f66a
Stephan Hoyer <shoyer@google.com>
2025-06-30 17:59:27 -0700

    Fix license and source-code URLs in PyPI metadata
    "Apache-2.0" is the standard SPDX string which PyPI prefers.
    
    "source-code" is the right way to indicate a GitHub link.
    
    PiperOrigin-RevId: 777795945

--- neuralgcm/__init__.py
+++ neuralgcm/__init__.py
@@ -44,4 +44,4 @@ import neuralgcm.legacy.towers
 
 from neuralgcm.legacy.api import PressureLevelModel
 
-__version__ = "1.2.0"  # keep in sync with pyproject.toml
+__version__ = "1.2.1"  # keep in sync with pyproject.toml
